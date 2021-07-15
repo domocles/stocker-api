d91b8da8815859cc25828a0644dde2f4d50543e5
domocles <sammy78_email@yahoo.co.uk>
2021-07-15 09:15:49 +0100

    Delivery
    Updated domain model to include a unique id as a natural id.  Annotated class with lombok annotations to create a builder and to automatically generate getters and setters.
    
      -  Added a builder, use Delivery.builder().build()
      -  Added a unique id of type UUID
      -  Used unique id to compare equals and hash
      -  Generate constructors, getters and setters via lombok
         -  All argument constructor
         -  No argument constructor

--- src/main/java/com/eep/stocker/domain/Delivery.java
+++ src/main/java/com/eep/stocker/domain/Delivery.java
@@ -1,88 +1,54 @@
 package com.eep.stocker.domain;
 
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Objects;
+import java.util.UUID;
 
 @Entity(name="delivery")
 @Table(name = "delivery")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Delivery {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "delivery_id")
     private Long id;
 
+    @NaturalId
+    @Column(name = "delivery_uid")
+    @Builder.Default
+    private UUID uid = UUID.randomUUID();
+
+    @Column(name = "delivery_supplier")
     @ManyToOne(fetch = FetchType.EAGER)
     private Supplier supplier;
 
+    @Column(name = "delivery_date")
     private Date deliveryDate;
 
+    @Column(name = "delivery_reference")
     private String reference;
 
+    @Column(name = "delivery_note")
     private String note;
-
-    public Delivery() {}
-
-    public Delivery(Supplier supplier, Date deliveryDate, String reference, String note) {
-        this.supplier = supplier;
-        this.deliveryDate = deliveryDate;
-        this.reference = reference;
-        this.note = note;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Supplier getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(Supplier supplier) {
-        this.supplier = supplier;
-    }
-
-    public Date getDeliveryDate() {
-        return deliveryDate;
-    }
-
-    public void setDeliveryDate(Date deliveryDate) {
-        this.deliveryDate = deliveryDate;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Delivery)) return false;
         Delivery delivery = (Delivery) o;
-        return Objects.equals(getSupplier(), delivery.getSupplier()) &&
-                Objects.equals(getDeliveryDate(), delivery.getDeliveryDate()) &&
-                Objects.equals(getReference(), delivery.getReference()) &&
-                Objects.equals(getNote(), delivery.getNote());
+        return Objects.equals(getUid(), delivery.getUid());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getSupplier(), getDeliveryDate(), getReference(), getNote());
+        return Objects.hash(getUid());
     }
 }
